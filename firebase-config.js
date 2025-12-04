8f36a64650dcf16bcb5b141acac2616a2029ea79
phuriberry <kingphurichayaaaaa@gmail.com>
2025-12-04 13:38:52 +0700

    อัปเดต Firebase config และเพิ่ม .gitignore

--- firebase-config.js
+++ firebase-config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     // ดูวิธี setup ในไฟล์ FIREBASE_SETUP.md
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
+    databaseURL: "https://score-system-sync-default-rtdb.asia-southeast1.firebasedatabase.app/",
     projectId: "YOUR_PROJECT_ID",
     storageBucket: "YOUR_PROJECT_ID.appspot.com",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
